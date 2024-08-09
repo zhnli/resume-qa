e843f5bc3c513d58a55d2f591f13068fa6159b2d
Zhen Li <seekstar@hotmail.com>
2024-08-09 04:35:03 +0000

    First commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,53 +1,95 @@
+
 import streamlit as st
 from openai import OpenAI
 
-# Show title and description.
-st.title("📄 Document question answering")
-st.write(
-    "Upload a document below and ask a question about it – GPT will answer! "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-)
+from pypdf import PdfReader
+from tempfile import NamedTemporaryFile
 
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
-else:
 
-    # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
+def init_state():
+    st.session_state['resume_text'] = ""
+    st.session_state.initialized = True
+    st.session_state.llm_response = None
 
-    # Let the user upload a file via `st.file_uploader`.
-    uploaded_file = st.file_uploader(
-        "Upload a document (.txt or .md)", type=("txt", "md")
+def read_pdf(pdf_path):
+    reader = PdfReader(pdf_path)
+    number_of_pages = len(reader.pages)
+    page = reader.pages[0]
+    text = page.extract_text()
+    return text
+
+def upload_callback():
+    uploaded_file = st.session_state['file_uploader']
+    if not uploaded_file or not uploaded_file.getbuffer():
+        return
+    with NamedTemporaryFile(dir='.', suffix='.pdf') as f:
+        f.write(uploaded_file.getbuffer())
+        st.session_state['resume_text'] = read_pdf(f.name)
+
+def submit_callback():
+    document = st.session_state.resume_text
+    messages = [
+        {
+            "role": "user",
+            "content": f"Given a resume below, ask 10 questions that a HR would most likely to ask. \n\n -- \n\n {document}",
+        }
+    ]
+
+    # Generate an answer using the OpenAI API.
+    stream = st.session_state.client.chat.completions.create(
+        model="gpt-4o", #"gpt-3.5-turbo",
+        messages=messages,
+        stream=True,
     )
 
-    # Ask the user for a question via `st.text_area`.
-    question = st.text_area(
-        "Now ask a question about the document!",
-        placeholder="Can you give me a short summary?",
-        disabled=not uploaded_file,
-    )
+    # Stream the response to the app using `st.write_stream`.
+    #st.write_stream(stream)
+    st.session_state.llm_response = stream
 
-    if uploaded_file and question:
+def page_main():
+    if 'initialized' not in st.session_state:
+        init_state()
 
-        # Process the uploaded file and question.
-        document = uploaded_file.read().decode()
-        messages = [
-            {
-                "role": "user",
-                "content": f"Here's a document: {document} \n\n---\n\n {question}",
-            }
-        ]
+    # Show title and description.
+    st.title("📄 Resume Deep Dive")
+    # st.write(
+    #     "Upload a document below and ask a question about it – GPT will answer! "
+    #     "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
+    # )
 
-        # Generate an answer using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=messages,
-            stream=True,
+    # Ask user for their OpenAI API key via `st.text_input`.
+    # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
+    # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
+    openai_api_key = st.text_input("OpenAI API Key", type="password")
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.", icon="🗝️")
+    else:
+
+        # Create an OpenAI client.
+        st.session_state.client = OpenAI(api_key=openai_api_key)
+
+        # Let the user upload a file via `st.file_uploader`.
+        uploaded_file = st.file_uploader(
+            "Upload a document (.txt or .md)",
+            type=("txt", "md", "pdf"),
+            on_change=upload_callback,
+            key='file_uploader'
         )
 
-        # Stream the response to the app using `st.write_stream`.
-        st.write_stream(stream)
+        resume_txt = st.text_area(
+            "Input text",
+            placeholder="Resume text",
+            key='resume_text'
+        )
+
+        question = st.text_area(
+            "Now ask a question about the document!",
+            placeholder="Given this resume, ask 10 questions that a HR would most likely to ask.",
+            disabled=not st.session_state.resume_text,
+        )
+
+        btn_submit = st.button("Submit", on_click=submit_callback, help="Submit the question")
+        if st.session_state.llm_response:
+            st.write_stream(st.session_state.llm_response)
+
+page_main()
